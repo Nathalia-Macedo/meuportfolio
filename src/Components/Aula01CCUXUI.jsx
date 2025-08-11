d8c06d57c497078132cf502ab99d6fd09ed11ca5
Nathalia-Macedo <nathaliademacedomartins04@gmail.com>
2025-08-11 19:18:31 -0300

    corrigindo erro do navigate

--- src/Components/Aula01CCUXUI.jsx
+++ src/Components/Aula01CCUXUI.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect,useNavigate } from "react"
 import {
   Moon,
   Sun,
@@ -20,7 +20,7 @@ import {
   Layout,
   ExternalLink,
 } from "lucide-react"
-
+navigate = useNavigate()
 const Button = ({ children, variant = "default", className = "", onClick, ...props }) => {
   const baseClasses =
     "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background"
